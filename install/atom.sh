6728d038cdbf4719a00d14efb3f6045b84a9ae24
Gwen Lofman <Gwen@Lofman.co>
2017-07-01 13:24:55 -0400

    Add Atom extensions to install script
    Mostly rust langage dependencies and theme and syntax theme I use.

--- install/atom.sh
+++ install/atom.sh
@@ -3,6 +3,23 @@
 URL="https://atom.io/download/deb"
 TEMP_FILE="/tmp/atom-install.deb"
 
+!(which atom) $$ echo "atom already installed.." &&
 sudo wget -O "$TEMP_FILE" "$URL" &&
 sudo dpkg -i --skip-same-version "$TEMP_FILE" &&
 sudo rm "$TEMP_FILE"
+
+PACKAGES=(
+  # Rust language support
+  "atom-language-rust"
+  "rust-doc-viewer"
+  "linter"
+  "linter-rust"
+  # Themes & Syntax themes
+  "github-atom-light-syntax"
+  "spacegray-light-neue-ui"
+)
+
+echo -e "\npackage installation..."
+for PACKAGE in ${PACKAGES[*]}; do
+  apm install $PACKAGE
+done
