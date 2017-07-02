6184cd2455fcea0061d294e987ebe1f0ec23dfb2
Gwen Lofman <Gwen@Lofman.co>
2017-07-02 00:38:04 -0400

    Move non-prompt related lines out of .prompt
    Move auto-completion related lines ot .env.

--- system/.env.sh
+++ system/.env.sh
@@ -1,3 +1,16 @@
+## Autocompletion
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
 ## History
 
 # don't put duplicate lines or lines starting with space in the history.
