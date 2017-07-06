106d97dfa6081903421696033ad4e8faad8117a7
Gwen Lofman <Gwen@Lofman.co>
2017-07-06 01:41:58 -0400

    Add update function to update packages
    Updates debian/system packages and packages specific to different
    package managers such as apm, npm, stack, and rustup

--- system/.functions.sh
+++ system/.functions.sh
@@ -15,3 +15,30 @@ web-install() {
 mkcd() {
     mkdir $1; cd $1
 }
+
+update-all() {
+  local nc='\033[0m'
+  local bgrn="\033[1;32m"
+
+  which rustup &&
+  echo -e "${bgrn}Updating rust toolchain${nc}...\n" &&
+  rustup update && rustup update nightly
+
+  which apm &&
+  echo -e "${bgrn}Updating atom packages${nc}...\n" &&
+  apm update && apm upgrade
+
+  which npm &&
+  echo -e "${bgrn}Updating node packages${nc}...\n" &&
+  alert "please enter password..." && sudo npm update
+
+  which stack &&
+  echo -e "${bgrn}Updating stack packages${nc}...\n" &&
+  stack upgrade && stack update
+
+  alert "please enter password"
+  echo -e "${bgrn}Updating system packages${nc}...\n"
+  sudo apt-get update &&
+  sudo apt-get -y upgrade &&
+  alert "finished installing updates"
+}
