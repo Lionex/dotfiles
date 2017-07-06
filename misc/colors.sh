1cfcdb9e96d74032016a60eb5e3ea4a75bf28c90
Gwen Lofman <Gwen@Lofman.co>
2017-07-06 01:41:09 -0400

    Add color reference file
    Stores lots of colors as variables

---
+++ misc/colors.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+NC='\033[0m' # Color Reset
+
+## Plain Colors
+
+BLK='\033[0;30m' # Black
+RED='\033[0;31m' # Red
+GRN='\033[0;32m' # Green
+YLW='\033[0;33m' # Yellow
+BLU='\033[0;34m' # Blue
+PUR='\033[0;35m' # Purple
+CYN='\033[0;36m' # Cyan
+WHT='\033[0;37m' # White
+
+## Bold Colors
+
+BBLK='\033[1;30m' # Black
+BRED='\033[1;31m' # Red
+BGRN='\033[1;32m' # Green
+BYLW='\033[1;33m' # Yellow
+BBLU='\033[1;34m' # Blue
+BPUR='\033[1;35m' # Purple
+BCYN='\033[1;36m' # Cyan
+BWHT='\033[1;37m' # White
+
+## Underlined Colors
+
+UBLK='\033[4;30m' # Black
+URED='\033[4;31m' # Red
+UGRN='\033[4;32m' # Green
+UYLW='\033[4;33m' # Yellow
+UBLU='\033[4;34m' # Blue
+UPUR='\033[4;35m' # Purple
+UCYN='\033[4;36m' # Cyan
+UWHT='\033[4;37m' # White
+
+## Background Colors
+
+GBLK='\033[40m' # Black
+GRED='\033[41m' # Red
+GGRN='\033[42m' # Green
+GYLW='\033[43m' # Yellow
+GBLU='\033[44m' # Blue
+GPUR='\033[45m' # Purple
+GCYN='\033[46m' # Cyan
+GWHT='\033[47m' # White
