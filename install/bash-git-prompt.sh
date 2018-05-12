60c605dbcd96b5e3fb1e923b84ba9b12be8c6121
Gwen Lofman <Gwen@Lofman.io>
2018-05-12 14:42:44 -0400

    Update install script for new shell and rust alts
    Install glitter and its dependencies to enable the shell prompt to
    work

--- install/bash-git-prompt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
--- install/rust.sh
+++ install/rust.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sudo curl https://sh.rustup.rs -sSf | sh
+
+sudo apt install pkg-config libssl-dev cmake
+
+cargo install glit exa
