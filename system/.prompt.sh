305371adedc53f11df2ef18a99b3631795ca317a
Gwen Lofman <Gwen@Lofman.io>
2019-04-14 14:10:36 -0400

    Use more spacious, minimal shell prompt
    Prompt uses more spaces as opposed to brackets and punctuation,
    reducing the visual noise surrounding the information presented.

--- system/.prompt.sh
+++ system/.prompt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export FMT="'## '#g;*(\b)#r(\B(#~('...')))\[#g;*(\+(#~('ahead ')))]\[#r;*(\-(#~('behind ')))]\(#g(\M\A\R\D)#r(\m\a\u\d))\{#m;*;_(\h('@'))}':'#y;*('\w')'\n\$ '"
+export GIT_FMT="#g;*(\b)#r(\B(' '))#g;*(\+(#~(' ahead ')))#r;*(\-(#~(' behind ')))' '#g(\M\A\R\D)#r(\m\a\u\d)\{#m;*;_(\h('@'))}' '#b;*('\w')'\n '"
 
-export ELSE_FMT="#g(#*('\u')'@\h')':'#b;*('\w')'\n\$ '"
+export PS1_FMT="#g;*('\h')' '#b;*('\w')'\n '"
 
 __set_prompt() {
   local EXIT="$?"
@@ -12,6 +12,6 @@ __set_prompt() {
   # If the last command didn't exit 0, display the exit code
   [ "$EXIT" -ne "0" ] && PS1+="$EXIT "
 
-  PS1+="$(glit "$FMT" -e "$ELSE_FMT")"
+  PS1+="$(glit "$GIT_FMT" -e "$PS1_FMT")"
 }
 export PROMPT_COMMAND=__set_prompt
