4efd8da2da37a94662a54a3881f63daaa0d90442
Gwen Lofman <Gwen@Lofman.io>
2020-10-17 12:54:18 -0400

    Make formatting more consistent

--- emacs/init.el
+++ emacs/init.el
@@ -113,11 +113,10 @@
   :custom
   (counsel-describe-function-function #'helpful-callable)
   (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
+  :bind (([remap describe-function] . counsel-describe-function)
+         ([remap describe-command] . helpful-command)
+         ([remap describe-variable] . counsel-describe-variable)
+         ([remap describe-key] . helpful-key)))
 
 ;; Project management
 
